26c8f40a5f612a0aa4b998e3b9c5cd8656d54f25
akanevrc <akanezoranomukou@gmail.com>
2023-11-11 11:15:19 +0900

    fix: generic_storeを修正

--- akaneclib/src/data/store/generic_store.rs
+++ akaneclib/src/data/store/generic_store.rs
@@ -64,6 +64,15 @@ where
     Key: Clone + Eq + Hash + Construct,
     Val: Clone,
 {
+    pub fn insert_new_vec(&mut self, key: Key) -> Result<()> {
+        if self.map.contains_key(&key) {
+            bail!(format!("Registration duplicated: `{}`", key.description()));
+        }
+        let vec = Rc::new(RefCell::new(Vec::new()));
+        self.map.insert(key.clone(), vec);
+        Ok(())
+    }
+
     pub fn push_into_vec(&mut self, key: Key, val: Val) {
         if !self.map.contains_key(&key) {
             let vec = Rc::new(RefCell::new(Vec::new()));
@@ -80,12 +89,27 @@ where
     InnerKey: Clone + Eq + Hash + Construct,
     Val: Clone,
 {
-    pub fn insert_into_map(&mut self, outer_key: OuterKey, inner_key: InnerKey, val: Val) {
+    pub fn insert_new_map(&mut self, outer_key: OuterKey) -> Result<()> {
+        if self.map.contains_key(&outer_key) {
+            bail!(format!("Registration duplicated: `{}`", outer_key.description()));
+        }
+        let map = Rc::new(RefCell::new(HashMap::new()));
+        self.map.insert(outer_key.clone(), map);
+        Ok(())
+    }
+
+    pub fn insert_into_map(&mut self, outer_key: OuterKey, inner_key: InnerKey, val: Val) -> Result<()> {
         if !self.map.contains_key(&outer_key) {
             let map = Rc::new(RefCell::new(HashMap::new()));
             self.map.insert(outer_key.clone(), map);
         }
-        let map = self.map.get(&outer_key).unwrap();
-        map.borrow_mut().insert(inner_key, val);
+        let mut map = self.map.get(&outer_key).unwrap().borrow_mut();
+        if map.contains_key(&inner_key) {
+            bail!(format!("Registration duplicated: `{}`", inner_key.description()));
+        }
+        else {
+            map.insert(inner_key, val);
+        }
+        Ok(())
     }
 }
